f862352bfcee85345a982334ce1d813d25ecb68e
Martin Paul Eve <martin@martineve.com>
2019-03-15 15:33:18 +0000

    Fix for non-debug

--- listBooks.py
+++ listBooks.py
@@ -48,7 +48,7 @@ logger = gogo.Gogo(
 
 
 def main(args):
-    if args['--debug']:
+    if '--debug' in args and args['--debug']:
         logger.setLevel('DEBUG')
     else:
         logger.setLevel('INFO')
